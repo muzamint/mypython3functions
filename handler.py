b7487d60a5b2bfc504b3e65cb20379aa5693fdc1
Ming-der Wang <ming@log4analytics.com>
2020-09-23 17:21:12 +0800

    fix data

--- handler.py
+++ handler.py
@@ -1,17 +1,24 @@
 from flask import Flask, request, jsonify
 import requests
 import json
+import datetime
+import sys
 
 from adapter import Adapter
 
 def handle(event, context):
     ploads = {'things':2,'total':25}
-    r = requests.get('https://httpbin.org/get',params=ploads)
+  #  r = requests.get('https://httpbin.org/get',params=ploads)
+    print(sys.version)
+    print(dir(context))
+    print(event)
+    x = datetime.datetime.now()
+    print("ming-now")
+    print(x)
     body = event["body"]
-    y = json.loads(body)
-    data = y
-    if data == '':
-        data = {}
+    body = "{\"id\": 0, \"data\": {\"base\": \"ETH\", \"quote\": \"USD\"}}"
+    data = json.loads(body)   
+    print(data)
     adapter = Adapter(data)
     return {
       "body": json.dumps(adapter.result, indent=2),
